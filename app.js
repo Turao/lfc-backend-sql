e1439ac8fafe0c5e2af58cfd54fc53970d2578c1
turao <lenz.arthur@gmail.com>
2019-02-24 09:37:55 -0300

    adding mocha, chai and chai-http to project

--- app.js
+++ app.js
@@ -9,7 +9,11 @@ const apiRouter = require('./routes/api-router');
 const app = express();
 
 app.use(cors());
-app.use(logger('dev'));
+
+if (process.env.NODE_ENV !== 'test') {
+  app.use(logger('dev'));
+}
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
